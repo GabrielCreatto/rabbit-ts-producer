d9f133d7a7881cd1d00149131b9f9de5224779e1
Gabriel Creatto <gabriel.creatto@portoseguro.com.br>
2023-01-04 18:38:24 -0300

    Change router methods

--- src/app/controllers/controller.ts
+++ src/app/controllers/controller.ts
@@ -1,12 +1,15 @@
 import { send } from '../service/producer';
-import { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 
-export function sendMessage(req: Request, res: Response) {
+const router = express.Router();
 
+router.get('/send', (req: Request, res: Response) => {
     const fila = req.query.queue ? req.query.queue.toString() : 'info';
 
     const teste_erro: boolean = req.query.teste_erro === 'true' ? true : false;
     send(fila, teste_erro);
 
-    res.send(`Sent message! Queue: ${fila}`);
-}
+    res.send(`Sent message to queue ${fila}!`);
+});
+
+export default router;
--- src/app/routes/route.ts
+++ src/app/routes/route.ts
@@ -1,13 +1,12 @@
-import express, { Express, Router } from 'express';
-import { sendMessage } from '../controllers/controller';
+import express, { Express, Router, Request, Response } from 'express';
+import router from '../controllers/controller';
 
 export async function init_routes() {
 
     const app: Express = express();
-    const router: Router = express.Router();
     const port: number = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-    router.get('/send', sendMessage);
+    app.use('/', router);
     await app.listen(port);
     console.log(`App startup! Listening on Port ${port}!`);
 
